871831471773c349c93eee4259f261ec87bd4784
cccswann <cccswann@gmail.com>
2021-01-24 18:04:01 -0800

    Add email regex

--- part_2.py
+++ part_2.py
@@ -9,7 +9,7 @@ phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
 mo = phoneNumRegex.search('My number is 415-555-4242.')
 print('Phone number found: '+ mo.group())
 
-### practice code 2
+### Ohone Number and Email Address Extractor
 import pyperclip, re
 
 phoneRegex = re.compile(r'''(
@@ -21,6 +21,14 @@ phoneRegex = re.compile(r'''(
     (\s*(ext|x|ext.)\s*(\d{2,5}))?
 )''', re.VERBOSE)
 
-# TODO: Create email regex.
+# Email regex
+
+emailRegex = re.compile(r'''(
+    [a-zA-Z0-9._%+-]+
+    @
+    [a-zA-Z0-9.-]+
+    (\.[a-zA-Z]{2,4})
+)''', re.VERBOSE)
+
 # TODO: Find matches in clipboard text.
 # TODO: Copy results to the clipboard.
